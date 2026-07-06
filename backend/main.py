664208781cceb49cee3c84403d1600f8bb603073
Harini Guggilla <122685164+Sweety327@users.noreply.github.com>
2026-07-06 20:52:39 +0530

    Add new allowed origin for CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="YouTube Spam Detection API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://spam-comments-detection-using-react.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
